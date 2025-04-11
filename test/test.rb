988943c1a37a00431e511afc6bc53814f213375d
Sunakshi Zutshi <zutshisunakshi@github.com>
2025-04-10 21:36:59 -0500

    Create test.rb

---
+++ test/test.rb
@@ -0,0 +1,23 @@
+# typed: true
+# frozen_string_literal: true
+
+require "csv"
+require "faraday"
+require "github/sendgrid/error"
+
+module Nurture
+  class SendgridImportJob < ApplicationJob
+
+    def perform(data)
+      return unless self.class.job_ff_enabled?
+      return unless self.class.env_vars_present?
+
+      if data.empty?
+        GitHub.logger.info(
+          "error.message" => "No contact emails sent to import job",
+        )
+
+        GitHub.dogstats.increment("nurture_campaign.sendgrid_import_job.error", tags: ["error:no_data"])
+        return
+      end
+end
